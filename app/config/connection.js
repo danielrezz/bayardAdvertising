bcb1e92bcf73dd4ba884fe829a49caeaf0a92540
Daniel Reza <daniel.reza1007@gmail.com>
2021-10-28 11:52:07 -0500

    updating heroku database

--- app/config/connection.js
+++ app/config/connection.js
@@ -2,10 +2,10 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "youshallnotpass!!",
-  database: "bayard",
+  host: "us-cdbr-east-04.cleardb.com",
+  user: "b3dedd81368b4b",
+  password: "0f727651",
+  database: "heroku_37b6e5b44e82a8e",
 });
 
 connection.connect(function (err) {
